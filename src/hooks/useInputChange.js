a909a729e219ed7d1a4bb6e8e0cd1a67680a34e8
wolfgrant <caman.developer@gmail.com>
2024-01-22 10:47:43 -0500

    fix: Se agrega ajustes para evitar inyección SQL

--- src/hooks/useInputChange.js
+++ src/hooks/useInputChange.js
@@ -1,19 +1,28 @@
 import { useState } from 'react';
 
+const cleanInput = (value) => {
+    const trimmedValue = value.trim();
+  
+    const sanitizedValue = trimmedValue.replace(/['";()]/g, '');
+  
+    return sanitizedValue;
+  };
+
 const useInputChange = (initialFormData) => {
+  const [formData, setFormData] = useState(initialFormData || {});
 
-    const [formData, setFormData] = useState(initialFormData || {})
+  const handleInputChange = (field, value) => {
+    const cleanedValue = cleanInput(value);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [field]: cleanedValue,
+    }));
+  };
 
-    const handleInputChange = (field, value) => {
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            [field]: value,
-        }));
-    }
-
-    return { 
-        handleInputChange,
-        formData};
+  return {
+    handleInputChange,
+    formData,
+  };
 };
 
 export default useInputChange;
