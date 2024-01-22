b77a72ebb8257f5f318fed9a4919b60fb5fa8fae
wolfgrant <caman.developer@gmail.com>
2024-01-22 10:34:03 -0500

    Fix: Se realiza ajuste del login

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    assetsInlineLimit: 10000,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
 })
